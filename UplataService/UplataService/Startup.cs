facf3595ef89c5caaa89fa11976efece2cf1e6bb
Dragana <d.zabaljac@gmail.com>
2022-02-17 19:54:34 +0100

    repositories, interfaces, initial swagger configuration

--- UplataService/UplataService/Startup.cs
+++ UplataService/UplataService/Startup.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
+using UplataService.Data;
+using UplataService.Entities;
 
 namespace UplataService
 {
@@ -19,6 +24,42 @@ namespace UplataService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<UplataDbContext>();
+
+            /*
+             * Singleton objects will be the same for all requests,
+             * Scoped objects will be the same during a request,
+             * Transient objects will be different every time
+             */
+            services.AddScoped<IKursRepository, KursRepository>();
+            services.AddScoped<IUplataRepository, UplataRepository>();
+
+            // Defining the generation of swagger
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Uplata service",
+                    Version = "1",
+                    Description = "Service for handling Uplatas.",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                    {
+                        Name = "Dragana Zabaljac",
+                        Email = "d.zabaljac@gmail.com"
+                    },
+                    License = new Microsoft.OpenApi.Models.OpenApiLicense
+                    {
+                        Name = "FTN licence",
+                        Url = new Uri("http://www.ftn.uns.ac.rs/")
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +68,14 @@ namespace UplataService
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    // Tells swaggerUI where to locate the swagger.json file
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    // Allows swagger to be accessible from the base route
+                    options.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
