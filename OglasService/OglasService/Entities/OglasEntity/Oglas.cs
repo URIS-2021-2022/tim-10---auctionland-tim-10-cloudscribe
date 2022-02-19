1a717a96019ac10b827b681877fe6c35738c278a
IrinaKuzmanovic <irinakuzmanovic@gmail.com>
2022-02-19 22:15:18 +0100

    obrisan liciter

--- OglasService/OglasService/Entities/OglasEntity/Oglas.cs
+++ OglasService/OglasService/Entities/OglasEntity/Oglas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,12 +9,14 @@ namespace OglasService.Entities
 {
     public class Oglas
     {
+        [Key]
         public Guid OglasId { get; set; }
 
         public string TekstOglasa { get; set; }
 
         [ForeignKey("sluzbeniListId")]
         public Guid SluzbeniListId { get; set; }
+        public SluzbeniList SluzbeniList { get; set; }
 
     }
 }
