f19da5509e9311ef93c885099639b534b9d6dde7
Andrijana Dragicevic <adragicevic99@gmail.com>
2022-02-22 19:11:44 +0100

    "Dodat ServiceCalls za Logger"

---
+++ Adresa/ServiceCalls/LoggerService.cs
@@ -0,0 +1,16 @@
+﻿using Adresa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adresa.ServiceCalls
+{
+    public class LoggerService : ILoggerService
+    {
+        public bool CreateLog(LoggerDto loggerDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
