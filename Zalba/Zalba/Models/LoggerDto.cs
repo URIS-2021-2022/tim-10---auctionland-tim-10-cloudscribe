ff0bd9d0e05854d513b133d5e81ce2e195bb6801
Dragana <d.zabaljac@gmail.com>
2022-02-22 23:00:01 +0100

    added logger and postman collection

---
+++ Zalba/Zalba/Models/LoggerDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZalbaService.Models
+{
+    /// <summary>
+    /// DTO class which will be used for LoggerService
+    /// </summary>
+    public class LoggerDto
+    {
+        /// <summary>
+        /// Date of the log
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// HTTP Method's response
+        /// </summary>
+        public string Response { get; set; }
+
+        /// <summary>
+        /// Exact HTTP Method
+        /// </summary>
+        public string HttpMethod { get; set; }
+
+        /// <summary>
+        /// Service name
+        /// </summary>
+        public string ServiceName { get; set; }
+
+        /// <summary>
+        /// Log level
+        /// </summary>
+
+        public string Level { get; set; }
+    }
+}
---
+++ Zalba/Zalba/ServiceCalls/ILoggerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZalbaService.Models;
+
+namespace ZalbaService.ServiceCalls
+{
+    /// <summary>
+    /// Interface for LoggerService
+    /// </summary>
+    public interface ILoggerService
+    {
+        /// <summary>
+        /// Definition of Logger's CreateLog method
+        /// </summary>
+        /// <param name="loggerDto">LoggerDto instance</param>
+        /// <returns></returns>
+        public bool CreateLog(LoggerDto loggerDto);
+    }
+}
