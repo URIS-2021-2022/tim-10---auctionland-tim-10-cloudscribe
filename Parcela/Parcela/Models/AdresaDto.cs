f0129eedb3721752d813e6cd06f33b3eec884dce
ChristianKlecina <djfntt@yahoo.com>
2022-02-22 11:09:18 +0100

    proba

---
+++ Parcela/Parcela/Models/AdresaDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcela.Models
+{
+    public class AdresaDto
+    {
+        /// <summary>
+        /// Ulica i broj adrese
+        /// </summary>
+        public string UlicaBroj { get; set; }
+
+        /// <summary>
+        /// Mesto adrese
+        /// </summary>
+        public string Mesto { get; set; }
+
+        /// <summary>
+        /// Poštanski broj adrese
+        /// </summary>
+        public int PostanskiBroj { get; set; }
+
+        /*/// <summary>
+        /// ID države
+        /// </summary>
+        public Guid DrzavaId { get; set; }*/
+
+        /// <summary>
+        /// Naziv države adrese
+        /// </summary>
+        public string NazivDrzave { get; set; }
+
+    }
+}
